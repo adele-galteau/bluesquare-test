0c8bb091edacea16ab52a0af1682ca43639ccdbd
Adèle <adele.galteaubresson@gmail.com>
2019-03-03 23:42:07 +0100

    set up redux store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './style/index.sass';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './style/index.sass'
+import App from './components/App'
+import * as serviceWorker from './serviceWorker'
+import createRootReducer from './reducers'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunkMiddleware from 'redux-thunk'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  createRootReducer(),
+  compose(
+    applyMiddleware(thunkMiddleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+)
+
+ReactDOM.render(<App store={store}/>, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
---
+++ src/reducers/exampleReducer.js
@@ -0,0 +1,3 @@
+export const exampleReducer = (state = "", action) => {
+  return state
+}
